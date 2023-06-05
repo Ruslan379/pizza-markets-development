d0e80041b5bac2aa3d5bb96d907f14aada55a803
Ruslan379 <arnov@i.ua>
2023-06-05 14:50:17 +0300

    Added buttons to decrease/ increase the quantity of pizzas

--- src/components/ShoppingCartOrder/ShoppingCartOrder.js
+++ src/components/ShoppingCartOrder/ShoppingCartOrder.js
@@ -5,7 +5,17 @@ import css from "./ShoppingCartOrder.module.css";
 
 export const ShoppingCartOrder = () => {
     const [value, setValue] = useState(true);
-    const togle = () => setValue(!value)
+    const togle = () => setValue(!value);
+
+    //! Тест кнопок Decrement и Increment
+    // const [value1, setValue1] = useState(0);
+    // const handleDecrement = () => {
+    //     setValue1(value1 - 1);
+    // };
+    // const handleIncrement = () => {
+    //     setValue1(value1 + 1);
+    // };
+
 
     const allChoicePizzasLocalStorage = JSON.parse(localStorage.getItem("allChoicePizzas"));
 
@@ -47,18 +57,56 @@ export const ShoppingCartOrder = () => {
                                     <p className={css.pricePizza}>{item.price * item.quantity} грн.</p>
 
                                     <div className={css.inputDeleteButtonContainer}>
-                                        <input className={css.inputQuantity}
+                                        <div className={css.inputContainer}>
+                                            <button
+                                                className="decrementButton"
+                                                // onClick={handleDecrement}
+                                                onClick={() => {
+                                                    let changeItemQuantity = item.quantity - 1;
+                                                    if (changeItemQuantity < 1) changeItemQuantity = 1; //! VBYBVFKMYJT значение quantity
+                                                    allChoicePizzasLocalStorage[index].quantity = changeItemQuantity;
+                                                    localStorage.setItem("allChoicePizzas", JSON.stringify([...allChoicePizzasLocalStorage]));
+                                                    togle();
+                                                }}
+                                            >
+                                                -
+                                            </button>
+                                            <input className={css.inputQuantity}
+                                                type="text"
+                                                name="quantity"
+                                                value={item.quantity}
+                                                readOnly
+                                            />
+                                            <button
+                                                className="incrementButton"
+                                                // onClick={handleIncrement}
+                                                onClick={() => {
+                                                    let changeItemQuantity = item.quantity + 1;
+                                                    if (changeItemQuantity > 50) changeItemQuantity = 50; //! МАКСИМАЛЬНОЕ значение quantity
+                                                    allChoicePizzasLocalStorage[index].quantity = changeItemQuantity;
+                                                    localStorage.setItem("allChoicePizzas", JSON.stringify([...allChoicePizzasLocalStorage]));
+                                                    togle();
+                                                }}
+                                            >
+                                                +
+                                            </button>
+                                        </div>
+
+                                        {/* //! OLD вариант инпута */}
+                                        {/* <input className={css.inputQuantity}
                                             type="number"
                                             name="quantity"
                                             min="1"
                                             max="100"
                                             defaultValue={item.quantity}
                                             onChange={(evt) => {
+                                                console.log(evt.target.value);
                                                 togle();
                                                 allChoicePizzasLocalStorage[index].quantity = Number(evt.target.value);
                                                 localStorage.setItem("allChoicePizzas", JSON.stringify([...allChoicePizzasLocalStorage]));
                                             }}
-                                        />
+                                        /> */}
+
                                         <button
                                             type="button"
                                             className={css.pizzaDeleteButton}
