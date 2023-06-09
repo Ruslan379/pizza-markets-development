e455acd53996b329638355e749b5c654131fc243
Ruslan379 <arnov@i.ua>
2023-06-09 03:35:33 +0300

    Added json file to load imagePizza by default-5

--- src/components/HomePizzasList/HomePizzasList.jsx
+++ src/components/HomePizzasList/HomePizzasList.jsx
@@ -1,5 +1,7 @@
 import pizzaMarketsJson from "db/pizzaMarketsMongoDB.json"; //!!! 
-import pictureDefault from "../../images/PizzasImages/01-01.PizzaMexico_VremenaGoda.png"; //!!! 
+
+// import pictureDefault from "../../images/PizzasImages/01-01.PizzaMexico_VremenaGoda.png"; //!!! 
+import pictureDefault from "../../images/free-icon-pizza-512-7467230.png"; //!!! 
 
 import css from "./HomePizzasList.module.css";
 
@@ -28,50 +30,50 @@ export const HomePizzasList = ({ shopIndex, allPizzas, addPizzaToCart }) => {
     return (
         <ul className={css.list}>
             {/* {allPizzas.map((item, index) => ( */}
-            {allPizzas.map(({pizza, picture = pictureDefault, price}, index) => (
-                <li
-                    className={css.listItem}
-                    // key={item.pizza}
-                    key={pizza}
-                >
-                    <img
-                        className={css.imagePizza}
-                        alt={"Pizza"}
-                        // src={imagePizza}
-                        // src={item.picture}
-                        src={picture}
-                        // src={pictureDefault}
-                        // src={picture || pictureDefault}
-                        // src={item.picture || pizzaMarketsJson[shopIndex].pizzas[index].defaultPicture}
-                        
-                        
-                        // src="../../images/PizzasImages/01-01.PizzaMexico_VremenaGoda.png"
-                        // src="https://pizzamexico.com.ua/wp-content/uploads/2020/02/4seasons.png"
-                        // src={pizzaMarketsJson[shopIndex].pizzas[index].picture}
-                        // src={pizzaMarketsJson[shopIndex].pizzas[index].defaultPicture}
-                        // src={process.env.REACT_APP_PUBLIC_URL + 'images/PizzasImages/01-01.PizzaMexico_VremenaGoda.png'}
-                        // src={imagePath}
-                        // src={imgSrc}
-                        width="100%"
-                    />
-                    <p className={css.namePizza}
-                    >
-                        {/* "{item.pizza}" */}
-                        "{pizza}"
-                        <span className={css.pricePizza}>
-                            {/* {item.price} грн. */}
-                            {price} грн.
-                        </span>
-                    </p>
-                    <button
-                        className={css.selectPizzaButton}
-                        type="button"
-                        onClick={() => addPizzaToCart(allPizzas[index])}
-                    >
-                        add to Cart
-                    </button>
-                </li>
-            ))}
+            {allPizzas.map(({ pizza, picture = pictureDefault, price }, index) => {
+                console.log("picture:", picture); //!
+                return <li
+                            className={css.listItem}
+                            // key={item.pizza}
+                            key={pizza}
+                        >
+                            <img
+                                className={css.imagePizza}
+                                alt={"Pizza"}
+                                // src={imagePizza}
+                                // src={item.picture}
+                                // src={picture}
+                                // src={pictureDefault}
+                                // src={picture || pictureDefault}
+                                // src={picture || pizzaMarketsJson[shopIndex].pizzas[index].defaultPicture}
+
+                                // src="../../images/PizzasImages/01-01.PizzaMexico_VremenaGoda.png"
+                                // src="https://pizzamexico.com.ua/wp-content/uploads/2020/02/4seasons.png"
+                                // src={pizzaMarketsJson[shopIndex].pizzas[index].picture}
+                                // src={pizzaMarketsJson[shopIndex].pizzas[index].defaultPicture}
+                                src={process.env.REACT_APP_PUBLIC_URL + 'images/PizzasImages/01-01.PizzaMexico_VremenaGoda.png'}
+                                // src={imagePath}
+                                // src={imgSrc}
+                                width="100%"
+                            />
+                            <p className={css.namePizza}
+                            >
+                                {/* "{item.pizza}" */}
+                                "{pizza}"
+                                <span className={css.pricePizza}>
+                                    {/* {item.price} грн. */}
+                                    {price} грн.
+                                </span>
+                            </p>
+                            <button
+                                className={css.selectPizzaButton}
+                                type="button"
+                                onClick={() => addPizzaToCart(allPizzas[index])}
+                            >
+                                add to Cart
+                            </button>
+                        </li>
+                })}
         </ul>
     );
 };
